a3660cd039adca28d5cd7f999c3764eb9140e502
Evgeniy Skuridin <redfield.ru@gmail.com>
2015-11-03 16:23:33 +0500

    Fix actions

--- src/js/actions.js
+++ src/js/actions.js
@@ -4,18 +4,20 @@ export const SORT_BY_TITLE = 'SORT_BY_TITLE';
 export const SORT_BY_PRICE = 'SORT_BY_PRICE';
 export const SORT_BY_AMOUNT = 'SORT_BY_AMOUNT';
 
-import store from './store';
-
 export function addItem(id) {
   return {
     type: ADD_ITEM,
-    item: store.getState().goods[id]
+    id: id
   };
 }
 
 export function removeItem(id) {
   return {
     type: REMOVE_ITEM,
-    itemId: id
+    id: id
   };
+}
+
+export function sort(type) {
+  return { type: type };
 }
